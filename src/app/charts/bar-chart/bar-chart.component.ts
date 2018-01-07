e16aa851906bec2c6d61acb3186338468739b382
Ram Sundaresan <Ramsundaresan@outlook.com>
2018-01-06 23:19:23 -0600

    Scafold Bar Chart - Bug Fixes

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 const SAMPLE_BARCHART_DATA : any[] = [
   {data:[12, 14, 18, 26, 40, 45, 49, 90],label:"Q3 Data"},
-  {data:[18, 26, 40, 45, 49, 90, 32, 31],label:"Q4 Data"},
+  {data:[18, 26, 40, 45, 49, 90, 32, 31],label:"Q4 Data"}
 ];
 
-const SAMPLE_BARCHART_LABEL : string[] = ["W1", "W2", "W3", "W4", "W5", "W6", "W7", "W8",]
+const SAMPLE_BARCHART_LABEL : string[] = ["W1", "W2", "W3", "W4", "W5", "W6", "W7", "W8"]
 
 
 @Component({
@@ -21,7 +21,7 @@ export class BarChartComponent implements OnInit {
   public barChartLabels : string[] = SAMPLE_BARCHART_LABEL;
   public barChartType ="bar";
   public barChartLegend = false; 
-  public barChartOptions : any {
+  public barChartOptions : any = {
     scaleShowVerticalLines:false,
     responsive:true
   };
